96cec6bdd4785ee362b90c2b70a133628d35037f
Kevin Bebel <kevin-bebel@users.noreply.github.com>
2016-06-05 14:57:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ angular.module('approve',[])
       var data = {
           "SupplierID" : supplierid,
           "CurrentParentRouteID" : "",
-          "TransactionID" : orderid,
+          "TransactionID" : orderid.toString(),
           "Status" : status,
           "RouteID" : "2",
           "Longitude" : "",
           "Latitude" : "",
           "WorkflowID" : workflowid,
-          "Comment" : status ? "appr_cust" : "rej_ cust"
+          "Comment" : status ? "appr_cust" : "rej_cust"
       }
 
       $.ajax({
